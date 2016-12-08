9f674a24ed7d755404e3ff676ed0b67f1ae2f694
kakakaya <kakakaya+git@gmail.com>
2016-12-08 12:47:08 +0900

    twitter.py: it works

--- boppy/adapter/twitter.py
+++ boppy/adapter/twitter.py
@@ -24,8 +24,7 @@ class TwitterStreamListener(tweepy.StreamListener):
         Keyword Arguments:
         status -- tweet
         """
-        print(status)
-        # self.queue.append(status)
+        self.queue.append(status)
 
     def on_error(self, status):
         print(status)
@@ -46,8 +45,8 @@ class TwitterInput(BaseInput):
         auth.set_access_token(access_key, access_sec)
         api = tweepy.API(auth)
         self.user = api.me()
-        self.stream = tweepy.Stream(auth=auth, listener=TwitterStreamListener(auth=auth, queue=self.queue))
-        # self.stream.filter(async=True)
+        self.stream = tweepy.Stream(auth=auth, listener=TwitterStreamListener(queue=self.queue))
+        self.stream.filter(follow=[str(self.user.id)], async=True)  # 認証したユーザーの出力を得る
 
     def __next__(self):
         while not self.queue:
@@ -65,44 +64,12 @@ class TwitterMessage(BaseMessage):
 
 # temp
 def main():
-    ck, cs = CONSUMER_KEY, CONSUMER_SEC
-    ak, as_ = ACCESS_KEY, ACCESS_SEC
-    auth = tweepy.OAuthHandler(ck, cs)
-    auth.set_access_token(ak, as_)
-    # api = tweepy.API(auth)
-    # user = api.me()
-    # print(user)
-
-    queue = []
-    s = TwitterStreamListener(queue=queue)
-    stream = tweepy.Stream(auth, s)
-    stream.filter(track=['python'])
-
-    """
-        ti = TwitterInput(CONSUMER_KEY, CONSUMER_SEC, ACCESS_KEY, ACCESS_SEC)
+    ti = TwitterInput(CONSUMER_KEY, CONSUMER_SEC, ACCESS_KEY, ACCESS_SEC)
     for i in ti:
         if not ti.stream.running:
             print("Stream dead")
+            break
         print(i.text())
-
-    ti.stream.filter(track=['python'])
-
-    """
-    # l = StdOutListener()
-
-    # stream = tweepy.Stream(auth, l)
-    # stream.filter(track=['basketball'])
-
-
-class StdOutListener(tweepy.StreamListener):
-    """ A listener handles tweets that are received from the stream.
-    This is a basic listener that just prints received tweets to stdout.
-    """
-    def on_data(self, data):
-        print(data)
-
-    def on_error(self, status):
-        print(status)
 
 
 if __name__ == "__main__":
