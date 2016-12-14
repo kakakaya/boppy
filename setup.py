615f1fd8e2f0ba5f814af79f008b0f3f8ca2622b
kakakaya <kakakaya+git@gmail.com>
2016-12-14 16:10:51 +0900

    added setup.py

---
+++ setup.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+# Author: kakakaya, Date: Wed Dec 14 16:00:22 2016
+from setuptools import setup, find_packages
+
+
+setup(
+    name="package_name",
+    version="1.0.0",
+    description='',
+    author='kakakaya',
+    packages=find_packages(),
+    test_suite="nose.collector",
+    install_requires=[
+    ],
+)
